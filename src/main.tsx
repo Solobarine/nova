47307d16b647c8c7684a9f4a61c6280206cb0095
Solly <solobarine@gmail.com>
2023-06-15 15:26:53 -0700

    Add cookies provider and redux store

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { CookiesProvider } from 'react-cookie'
 import App from './App.tsx'
+import store from './features/store/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <CookiesProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </CookiesProvider>
   </React.StrictMode>,
 )
