59b1e1e03b6761708f1cfc5f787d34c9ce62b11b
Solly <solobarine@gmail.com>
2023-06-15 15:35:47 -0700

    Create async thunks for retrieving all series and a single series

---
+++ src/features/async_thunks/Series.ts
@@ -0,0 +1,25 @@
+import { createAsyncThunk } from "@reduxjs/toolkit/dist/createAsyncThunk"
+import getRequest from "../../utils/getRequest"
+
+const all_series = () => {
+    const url: string = 'https://api.tvmaze.com/shows'
+    return getRequest(url)
+}
+
+const single_series = (payload: number) => {
+    const url: string = `https://api.tvmaze.com/shows${payload}?embed=cast`
+    return getRequest(url)
+}
+
+const series = {
+    get_series: createAsyncThunk(
+        'GetAllSeries',
+        all_series
+    ),
+    single_series: createAsyncThunk(
+        'GetSingleSeries',
+        single_series
+    )
+}
+
+export default series
