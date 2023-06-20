a3754e88e8ba91cb722d9c6df91fd3784f43d2de
Solly <solobarine@gmail.com>
2023-06-20 14:19:16 +0100

    Add try-catch block for better error handling

--- src/utils/getRequest.ts
+++ src/utils/getRequest.ts
@@ -1,8 +1,13 @@
 const getRequest = async(url: string) => {
-    const response = await fetch(url)
-    const { status } = response
-    const data = response.json()
-    return { data, status }
+    try {
+        const response = await fetch(url)
+        const { status } = response
+        const data = await response.json()
+        const json = {data, status}
+        return json  
+    } catch (error: any) {
+       return error
+    }
 }
 
 export default getRequest
