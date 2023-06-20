a7b73ca1648f538c7a4ef2b76b2f8c539f5d895e
Solly <solobarine@gmail.com>
2023-06-20 14:13:24 +0100

    Add Top Media Services

---
+++ src/media_service.ts
@@ -0,0 +1,39 @@
+const media_service = [
+    {
+        id: 0,
+        name: 'Apple TV +',
+        icon: '/media/apple_tv.svg'
+    },
+    {
+        id: 1,
+        name: 'HBO Max',
+        icon: '/media/hbo.svg'
+    },
+    {
+        id: 2,
+        name: 'Peacock',
+        icon: '/media/peacock.svg'
+    },
+    {
+        id: 3,
+        name: 'Disney +',
+        icon: '/media/disney_plus.svg'
+    },
+    {
+        id: 4,
+        name: 'Hulu',
+        icon: '/media/hulu.svg'
+    },
+    {
+        id: 5,
+        name: 'Netflix',
+        icon: '/media/netflix.svg'
+    },
+    {
+        id: 6,
+        name: 'Prime',
+        icon: '/media/prime.svg'
+    }
+]
+
+export default media_service
