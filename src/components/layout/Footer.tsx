46a4c89ac045d828786a4390a613ffac2084f4f1
Solly <solobarine@gmail.com>
2023-06-20 14:40:57 +0100

    Create Footer Component

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,18 @@
+import './css/Footer.css'
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+          <i className="fa-brands fa-facebook"></i>
+          <i className="fa-brands fa-instagram"></i>
+          <i className="fa-brands fa-youtube"></i>
+          <i className="fa-brands fa-linkedin-in"></i>
+          <i className="fa-brands fa-twitter"></i>
+          <i className="fa-brands fa-pinterest"></i>
+        </div>
+        <p id="copyright">&copy; 2023 Solly. All Rights Reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
